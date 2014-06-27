cd0ae432c59c06d385dda62a3d187415fde955da
Joseph L <jxrlee@gmail.com>
2014-06-27 10:01:54 -0700

    First commit

---
+++ canvas/JLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JLAppDelegate.h
+//  canvas
+//
+//  Created by Joseph Lee on 6/27/14.
+//  Copyright (c) 2014 mn8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ canvasTests/canvasTests.m
@@ -0,0 +1,34 @@
+//
+//  canvasTests.m
+//  canvasTests
+//
+//  Created by Joseph Lee on 6/27/14.
+//  Copyright (c) 2014 mn8. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface canvasTests : XCTestCase
+
+@end
+
+@implementation canvasTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
